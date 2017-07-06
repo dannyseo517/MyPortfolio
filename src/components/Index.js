6a58e362da307034cd331b755a4de9f49fc0e06e
kaniseo <dannyseo517@gmail.com>
2017-07-05 23:48:46 -0700

    fixed scroll event

--- src/components/Index.js
+++ src/components/Index.js
@@ -3,15 +3,25 @@ import React, {Component} from 'react';
 const lol = {
 	margin: 0
 }
+const menu = {
+	left: '0px'
+}
 var lastScrollPos = 0;
 
 class Index extends Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			pos: 0
+		}
+		this.handleScroll = this.handleScroll.bind(this);
 	}
 	handleScroll(event){
 		if(lastScrollPos < window.pageYOffset){
 			console.log('scroll down');
+			let _pos = this.state.pos + 1;
+			this.setState({pos: _pos})
+			console.log(this.state.pos);
 		}else{
 			console.log('scroll up');
 		}
@@ -23,6 +33,7 @@ class Index extends Component{
 	render(){
 		return(
 			<div>
+				<div style={{marginLeft: this.state.pos}}>this</div>
 				<div style={{margin:'1000px'}}>fuckk</div>
 				<div>lol</div>
 			</div>
